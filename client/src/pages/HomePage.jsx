3165bde7a2f7840cde52a27c7f0505dfd28f3644
Yugal <Yugal@coder.com>
2024-06-13 14:57:25 +0530

    add

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate hook for navigation
+
+const HomePage = () => {
+  const [tasks, setTasks] = useState([]);
+  const navigate = useNavigate(); // Initialize the useNavigate hook
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await axios.get('http://localhost:3000/server/tasks/getAllTask', { withCredentials: true });
+        setTasks(response.data);
+      } catch (error) {
+        console.error('Error fetching tasks:', error.message);
+      }
+    };
+
+    fetchTasks();
+  }, []);
+
+
+  return (
+    <div className='task-box'>
+      <div className='color'>
+        <div className='blue'>
+            <h1>Highest Priority</h1>
+        </div>
+        <div className='brown'>
+            <h1>Medium Priority</h1>
+        </div>
+        <div className='red'>
+            <h1>Low Priority</h1>
+        </div>
+     </div>
+      
+      <h1>Tasks</h1>
+      <ul>
+        {tasks.map(task => (
+          <li key={task._id}>
+            <div className='particuler-task'>
+              <span className='left-task' style={{ color: getPriorityColor(task.priority) }}>
+                {task.title} - {task.status} - {task.dueDate}
+              </span>
+              <div className='right-button'>
+                <Link to={`/task/${task._id}`}>
+                  <button>Read more</button>
+              </Link>
+              </div>
+
+            </div>
+          </li>
+        ))}
+      </ul>
+      <h1>Login to Create New Task</h1>
+    </div>
+  );
+};
+
+const getPriorityColor = (priority) => {
+  switch (priority) {
+    case 'low':
+      return 'green';
+    case 'medium':
+      return 'orange';
+    case 'high':
+      return 'red';
+    default:
+      return 'black';
+  }
+};
+
+export default HomePage;
